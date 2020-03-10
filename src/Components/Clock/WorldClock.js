8c6e8a283f5f62620a3bb77d9e8c52d32b38cf3b
LucaDantas <luca100sdantas2@gmail.com>
2020-03-10 17:47:11 -0300

    Conectar api externa world clock

--- src/Components/Clock/WorldClock.js
+++ src/Components/Clock/WorldClock.js
@@ -2,35 +2,72 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class WorldClock extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        date: new Date(),
-      };
-    }
-    componentDidMount() {
-      this.timerID = setInterval(
-        () => this.tick(),
-        1000
-      );
-    }
-  
-    tick() {
-      this.setState({
-        date: new Date(),
-      });
-      this.state = {date: new Date};
-    }
-  
-    render() {
-      let {date, local} = this.state;
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      date: new Date(),
+    };
+  }
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  tick() {
+    fetch("http://worldtimeapi.org/api/timezone/America/Fortaleza")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            error: null,
+            isLoaded: true,
+            date: new Date(result.datetime)
+          });
+        },
+        // Nota: É importante lidar com os erros aqui
+        // em vez de um bloco catch() para não recebermos
+        // exceções de erros dos componentes.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+    //codigo    
+    //this.setState({
+    //date: new Date(),
+    //isLoaded: true
+    //});
+    //this.state = { date: new Date };
+  }
+
+  render() {
+    let { isLoaded, date, error } = this.state;
+    if (error) {
       return (
         <div className="main">
-          <h2>World clock is{date.toLocaleTimeString()}.</h2>
-          <p>{local}</p>
-        </div>
-      );
+          <h2>World clock is :/ ({error})</h2>
+        </div>)
+
+    }
+    else if (!isLoaded) {
+      return (
+        <div className="main">
+          <h2>World clock is({error})</h2>
+        </div>)
+    }
+    else {
+      return (
+        <div className="main">
+          <h2>World clock is ({error})</h2>
+        </div>)
     }
   }
-  
-  export default WorldClock;
+}
+
+export default WorldClock;
